6fcc1b9a5df6f292bd4f08000408bf4303889d83
Fousseynou Sakho <exemple@exemple.fr>
2018-10-26 12:35:14 +0200

    modification du fichier pour tester le sonar

--- src/main/java/org/mybatis/jpetstore/service/AccountService.java
+++ src/main/java/org/mybatis/jpetstore/service/AccountService.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2010-2017 the original author or authors.
+ *    Copyright 2010-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -62,7 +62,7 @@ public class AccountService {
     accountMapper.updateAccount(account);
     accountMapper.updateProfile(account);
 
-    if (account.getPassword() != null && account.getPassword().length() > 0) {
+    if (account != null && account.getPassword().length() > 0) {
       accountMapper.updateSignon(account);
     }
   }
